1ef28a90f901b466def0873d280acc1883831ddc
Ardhenis Muhammad Aflah <aflahdenis9@gmail.com>
2024-12-25 12:56:46 +0700

    Penambahan array static (LEVEL)
    penambahan bertujuan menambahkan fleksibilitas dalam mengembangkan gameplay mekanik, khususnya untuk randomize level, saat ini level sudah dipilih otomatis, NOTE: setiap level perlu disetting level berapa pada substance dan bad substance serta endgamemanager

--- Assets/Code/StaticInformation/LevelMission.cs
+++ Assets/Code/StaticInformation/LevelMission.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelMission : MonoBehaviour
-{
-   // level -  {bad, good}
+{  //          {0,   1,     2}
+   // level -  {bad, good, goodStock}
    // ini mendeskripsikan berapa banyak target yang diperlukan (jika bad = sekian maka end lose game)
      public static Dictionary<int, int[]> objective = new Dictionary<int, int[]>()
      {
-        {1, new int[] {10,4}},
-        {2, new int[] {2,3}}
+        {1, new int[] {10,4, 10}},
+        {2, new int[] {2,8, 20}}
      };
 }
